75d9f4b4be34537f589635f7adc7b77314e214c3
Kien Lai <kiencl@uci.edu>
2017-08-03 09:52:55 -0700

    post layouts

--- templates/post/content-row-full.php
+++ templates/post/content-row-full.php
@@ -6,3 +6,33 @@
  * Time: 1:44 PM
  */
 echo 'ROW FULL';
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>content-row-full</title>
+	<link rel="stylesheet" href="../../assets/theme-styles/uciseventeen.css">
+</head>
+<body>
+	<!-- header goes here -->
+	<main>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<img class="img-responsive" src="https://news.uci.edu/wp-content/uploads/2017/07/hidden-594142_1440-1.jpg" alt="image">
+					<h1>Title</h1>
+					<p>Content</p>
+				</div>
+				<aside class="col-md-12">
+					<h4>Aside</h4>
+				</aside>
+			</div>
+		</div>
+	</main>
+
+</body>
+</html>
