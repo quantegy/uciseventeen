f0367cf68ae4d5a32aff5cce13549c38e5ccb03e
Kien Lai <kiencl@uci.edu>
2017-08-18 13:47:50 -0700

    corrected misspelling of title

--- templates/post/home-default.php
+++ templates/post/home-default.php
@@ -15,7 +15,7 @@ echo 'HOME DEFAULT';
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<title>Homme Default</title>
+		<title>Home Default</title>
 		<link rel="stylesheet" href="../../assets/theme-styles/uciseventeen.css">
 	</head>
 
