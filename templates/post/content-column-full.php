414eee4a2908bf4d753229dad74270c62a195025
Christopher Walsh <walshcj@uci.edu>
2017-11-28 09:53:53 -0800

    Adding captions to post templates

--- templates/post/content-column-full.php
+++ templates/post/content-column-full.php
@@ -3,6 +3,7 @@
         <div class="row">
             <div class="col-md-8">
 				<?php the_post_thumbnail( 'full', [ 'class' => 'img-responsive' ] ); ?>
+                <div class="caption"><?php the_post_thumbnail_caption(); ?></div>
                 <h1><?php the_title(); ?></h1>
                 <p><?php the_content(); ?></p>
             </div>
